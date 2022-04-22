080df87c64d297e4c49438d1732636a1b2cee9af
efdee1 <63516254+efdee1@users.noreply.github.com>
2022-04-22 10:28:20 +0100

    get func

--- get_func.c
+++ get_func.c
@@ -1,26 +1,58 @@
 #include "main.h"
 /**
-* get_func - search and return the correct function
-* @s: array to check
-* Return: return a function (if written correctly)
-**/
-int (*get_func(char s))(va_list)
+ * get_print_func - selects the correct function to perform the operation.
+ * @s: argument indentifier
+ * @index: index for argument indentifier
+ * Return: pointer to a function.
+ */
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 {
-	op_t ops[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"d", print_d},
-		{"i", print_i},
-		{"b", print_b},
-		{NULL, NULL}
+	print_t pr[] = {
+		{"c", print_chr}, {"s", print_str},
+		{"i", print_int}, {"d", print_int},
+		{"b", print_bnr}, {"u", print_unt},
+		{"o", print_oct}, {"x", print_hex},
+		{"X", print_upx}, {"S", print_usr},
+		{"p", print_add}, {"li", prinlint},
+		{"ld", prinlint}, {"lu", prinlunt},
+		{"lo", prinloct}, {"lx", prinlhex},
+		{"lX", prinlupx}, {"hi", prinhint},
+		{"hd", prinhint}, {"hu", prinhunt},
+		{"ho", prinhoct}, {"hx", prinhhex},
+		{"hX", prinhupx}, {"#o", prinnoct},
+		{"#x", prinnhex}, {"#X", prinnupx},
+		{"#i", print_int}, {"#d", print_int},
+		{"#u", print_unt}, {"+i", prinpint},
+		{"+d", prinpint}, {"+u", print_unt},
+		{"+o", print_oct}, {"+x", print_hex},
+		{"+X", print_upx}, {" i", prinsint},
+		{" d", prinsint}, {" u", print_unt},
+		{" o", print_oct}, {" x", print_hex},
+		{" X", print_upx}, {"R", print_rot},
+		{"r", print_rev}, {"%", print_prg},
+		{"l", print_prg}, {"h", print_prg},
+		{" +i", prinpint}, {" +d", prinpint},
+		{"+ i", prinpint}, {"+ d", prinpint},
+		{" %", print_prg}, {NULL, NULL},
 	};
+	int i = 0, j = 0, first_index;
 
-	int i;
-
-	for (i = 0; ops[i].c != NULL; i++)
+	first_index = index;
+	while (pr[i].type_arg)
 	{
-		if (*ops[i].c == s)
-			return (ops[i].f);
+		if (s[index] == pr[i].type_arg[j])
+		{
+			if (pr[i].type_arg[j + 1] != '\0')
+				index++, j++;
+			else
+				break;
+		}
+		else
+		{
+			j = 0;
+			i++;
+			index = first_index;
+		}
 	}
-	return (NULL);
+	return (pr[i].f);
 }
